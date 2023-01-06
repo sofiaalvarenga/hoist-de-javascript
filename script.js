cbf0590f685a16d0f3198f2ff32b7abf6c95e861
Sofia Alvarenga Palmerola <srap1305@gmail.com>
2023-01-06 11:30:20 -0300

    hoisting

--- script.js
+++ script.js
@@ -1,125 +1,81 @@
-// EJEMPLO
-console.log(example);
-var example = "I'm the example!";
-// var example;
-// console.log(example); // logs undefined
-// example = "I'm the example!";
-
 //1
-console.log(hello);
-var hello = 'world';
-// var hello;
-// console.log(example); // imprime undefined
-// hello = 'world';
+var hello; //se crea variable 
+console.log(hello); // imprime undefined
+hello = 'world';
 
 //2
-var needle = 'haystack';
-test();
+var needle; //variable global
+needle = 'haystack'; 
+test(); // se invoca la function test()
 function test(){
-    var needle = 'magnet';
-    console.log(needle);
+    var needle;  //variable local
+    needle = 'magnet';
+    console.log(needle);  // Imprime 'magnet';
 }
-// var needle
-// needle = 'haystack' *variable global*
-// se invoca la function test()
-// function test
-// needle = 'magnet' *variable local*
-// console.log(needle) // Imprime 'magnet';
 
 //3
-var brendan = 'super cool';
-function print(){
-    brendan = 'only okay';
+var brendan; //variable global
+brendan = 'super cool'; 
+function print(){ //no se invoca la función previamente
+    var brendan; // variable local
+    brendan = 'only okay'; 
     console.log(brendan);
 }
-console.log(brendan);
-// var brendan
-// brendan = 'super cool' *variable global*
-// function print()
-// brendan = 'only okay' *variable local*
-// console.log (brendan) // imprime 'super cool'
+console.log(brendan);  // imprime 'super cool'
 
 //4
-var food = 'chicken';
-console.log(food);
-eat();
+var food; // variable global
+food = 'chicken';
+console.log(food); // imprime 'chicken'
+eat(); //se invoca la función eat()
 function eat(){
-    food = 'half-chicken';
-    console.log(food);
+    var food; // variable local
+    food = 'half-chicken'; 
+    console.log(food); // imrpime 'half-chicken'
     var food = 'gone';
 }
-// var food
-// food = 'chicken' *variable global*
-// console.log (food) // imprime 'chicken'
-// función eat() se invoca
-// function eat()
-// food = 'half-chicken' *variable local*
-// console.log(food) // imrpime 'half-chicken'
-// food = 'gone'
 
 //5
-mean();
-console.log(food);
-var mean = function() {
+var food; //se crea variable 
+var mean; //se crea variable 
+mean(); //mean no existe como función
+console.log(food); // food no está definida fuera de function()
+mean = function() {
+    var food; //variable local
     food = "chicken";
     console.log(food);
-    var food = "fish";
+    food = "fish";
     console.log(food);
 }
-console.log(food);
-// var mean
-// var food
-// se invoca la función mean () // no es un función
-// console.log (food)// food no está definida
-// mean = funtion()
-// var food 
-// food = "chicken"
-// console.log (food)
-// food = "fish"
-// console.log (food)// food no está definida
+console.log(food); // food no está definida fuera de function()
 
 //6
-console.log(genre);
-var genre = "disco";
-rewind();
+var genre;
+console.log(genre); // imrpime indefined
+genre = "disco"; //variable global
+rewind(); //se invoca función rewind ()
 function rewind() {
-    genre = "rock";
-    console.log(genre);
-    var genre = "r&b";
-    console.log(genre);
+    var genre; //variable local
+    genre = "rock"; 
+    console.log(genre); // imprime "rock"
+    genre = "r&b"; 
+    console.log(genre); // imprime "r&b"
 }
-console.log(genre);
-// var genre
-// console.log(genre)// imrpime indefined
-// genre = "disco" *variable global*
-// se invoca función rewind()
-// function rewind()
-// genre = "rock" *variable local*
-// console.log(genre)// imprime "rock"
-// genre = "r&b" *variable local*
-// console.log(genre)// imprime "r&b"
-// console.log(genre)// imrpime "disco"
+console.log(genre); // imrpime "disco"
 
 //7
+var dojo; //variable global
 dojo = "san jose";
-console.log(dojo);
-learn();
+console.log(dojo); // imprime "san jose"
+learn(); //se invoca la función learn()
 function learn() {
+    var dojo; //variable local
     dojo = "seattle";
-    console.log(dojo);
-    var dojo = "burbank";
-    console.log(dojo);
+    console.log(dojo); // imprime "seatle"
+    dojo = "burbank";
+    console.log(dojo); // imprime "burbank"
 }
-console.log(dojo);
-// var dojo 
-// dojo = "san jose" *variable global*
-// se invoca función learn()
-// function learn()
-// dojo "seatle" *variable local*
-// console.log(dojo) // imprime "seatle"
-// dojo = "burbank" *variable local*
-// console.log (dojo)// imprime "burbank"
-// console.log (dojo)// imprime "san jose"
+console.log(dojo); // imprime "san jose"
 
 //8
 console.log(makeDojo("Chicago", 65));
