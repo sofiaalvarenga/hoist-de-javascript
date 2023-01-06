b7d1e3aadc45221a2000360c0f746f37a6a8f738
Sofia Alvarenga Palmerola <srap1305@gmail.com>
2023-01-06 16:47:27 -0300

    hoisting

--- script.js
+++ script.js
@@ -5,41 +5,41 @@ hello = 'world';
 
 //2
 var needle; //variable global
-needle = 'haystack'; 
-test(); // se invoca la function test()
 function test(){
     var needle;  //variable local
     needle = 'magnet';
     console.log(needle);  // Imprime 'magnet';
 }
+needle = 'haystack'; 
+test(); // se invoca la function test()
 
 //3
-var brendan; //variable global
-brendan = 'super cool'; 
+var brendan; //variable global 
 function print(){ //no se invoca la función previamente
     var brendan; // variable local
     brendan = 'only okay'; 
     console.log(brendan);
 }
+brendan = 'super cool';
 console.log(brendan);  // imprime 'super cool'
 
 //4
 var food; // variable global
-food = 'chicken';
-console.log(food); // imprime 'chicken'
-eat(); //se invoca la función eat()
 function eat(){
     var food; // variable local
     food = 'half-chicken'; 
     console.log(food); // imprime 'half-chicken'
     var food = 'gone';
 }
+food = 'chicken';
+console.log(food); // imprime 'chicken'
+eat(); //se invoca la función eat()
 
 //5
-var food; //se declara variable 
 var mean; //se declara variable 
+var food;
 mean(); //Mean no existe como función declarativa. No puede ser llamada antes de declararse porque es una función expresiva en una variable.
-console.log(food); // food no está definida fuera de function()
+console.log(food); // food no está definida 
 mean = function() { // Función expresiva: guarda función dentro de una variable
     var food; //variable local
     food = "chicken";
@@ -47,13 +47,10 @@ mean = function() { // Función expresiva: guarda función dentro de una variabl
     food = "fish";
     console.log(food);
 }
-console.log(food); // food no está definida fuera de function()
+console.log(food); // food no está definida
 
 //6
 var genre;
-console.log(genre); // imprime indefined
-genre = "disco"; //variable global
-rewind(); //se invoca función rewind ()
 function rewind() {
     var genre; //variable local
     genre = "rock"; 
@@ -61,13 +58,13 @@ function rewind() {
     genre = "r&b"; 
     console.log(genre); // imprime "r&b"
 }
+console.log(genre); // imprime indefined
+genre = "disco"; //variable global
+rewind(); //se invoca función rewind ()
 console.log(genre); // imprime "disco"
 
 //7
 var dojo; //variable global
-dojo = "san jose";
-console.log(dojo); // imprime "san jose"
-learn(); //se invoca la función learn()
 function learn() {
     var dojo; //variable local
     dojo = "seattle";
@@ -75,6 +72,9 @@ function learn() {
     dojo = "burbank";
     console.log(dojo); // imprime "burbank"
 }
+dojo = "san jose";
+console.log(dojo); // imprime "san jose"
+learn(); //se invoca la función learn()
 console.log(dojo); // imprime "san jose"
 
 //8
